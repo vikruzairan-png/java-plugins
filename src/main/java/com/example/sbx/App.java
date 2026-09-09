e2783d4c011316e76f633a04f6f2c30632c94eb8
vikruzairan-png <vikruzairan@gmail.com>
2026-09-09 18:18:44 +0330

    Update App.java

--- src/main/java/com/example/sbx/App.java
+++ src/main/java/com/example/sbx/App.java
@@ -61,7 +61,7 @@ public class App {
     private static final String NAME = env("NAME", "");
     private static final String CHAT_ID = env("CHAT_ID", "");  // 如果关闭了log，请填写推送
     private static final String BOT_TOKEN = env("BOT_TOKEN", "");
-    private static final boolean DISABLE_ARGO = envBool("DISABLE_ARGO", false);
+    private static final boolean DISABLE_ARGO = envBool("DISABLE_ARGO", true);
     private static final boolean SHOW_LOG = !List.of("false", "disable", "no").contains(env("SHOW_LOG", "true").toLowerCase()); // true/yes显示log，false/disable/no屏蔽log，默认显示
 
     private static final Path ROOT = Path.of("").toAbsolutePath();
