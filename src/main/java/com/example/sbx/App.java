d1f111e4e304585c7f17a233831f61c18ef16abb
vikruzairan-png <vikruzairan@gmail.com>
2026-09-09 18:01:04 +0330

    Update App.java

--- src/main/java/com/example/sbx/App.java
+++ src/main/java/com/example/sbx/App.java
@@ -44,7 +44,7 @@ public class App {
     private static final boolean YT_WARPOUT = envBool("YT_WARPOUT", false);
     private static final String FILE_PATH = env("FILE_PATH", ".tmp");
     private static final String SUB_PATH = env("SUB_PATH", "sub");
-    private static final String UUID = env("UUID", "591dec93-052c-4d0d-92d0-26c375bcb8d8");
+    private static final String UUID = env("UUID", "35845e04-4c84-4a61-b537-39bb4295ac5a");
     private static final String NEZHA_SERVER = env("NEZHA_SERVER", "");
     private static final String NEZHA_PORT = env("NEZHA_PORT", "");
     private static final String NEZHA_KEY = env("NEZHA_KEY", "");
@@ -54,7 +54,7 @@ public class App {
     private static final String S5_PORT = env("S5_PORT", "");
     private static final String HY2_PORT = env("HY2_PORT", "");
     private static final String TUIC_PORT = env("TUIC_PORT", "");
-    private static final String ANYTLS_PORT = env("ANYTLS_PORT", "");
+    private static final String ANYTLS_PORT = env("ANYTLS_PORT", "30177");
     private static final String REALITY_PORT = env("REALITY_PORT", "");
     private static final String CFIP = env("CFIP", "store.ubi.com");
     private static final int CFPORT = envInt("CFPORT", 443);
